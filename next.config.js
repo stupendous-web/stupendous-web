5efae272adc74b0f68be2a0a678ab917e98eed71
stupendous-web <topher@stupendousweb.com>
2022-07-04 15:29:59 -0700

    Think twice, code once.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  siteUrl: "https://stupendousweb.com",
+  generateRobotsTxt: true,
 };
 
 module.exports = nextConfig;
