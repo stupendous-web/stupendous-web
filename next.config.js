1b035f09650c6dc3c6afeb4b013478f5443657c0
stupendous-web <topher@stupendousweb.com>
2022-07-04 16:21:51 -0700

    Think twice, code once.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const axios = require("axios");
+
 const nextConfig = {
   reactStrictMode: true,
   siteUrl: "https://stupendousweb.com",
   generateRobotsTxt: true,
+  additionalPaths: async (config) => {
+    const result = [];
+    await axios
+      .get("https://cms.stupendousweb.com/wp-json/wp/v2/posts")
+      .then((response) => {
+        response.data.map((post) => {
+          result.push({
+            loc: "/article/" + post.slug,
+            changefreq: "daily",
+            priority: 0.7,
+            lastmod: new Date().toISOString(),
+          });
+        });
+      });
+    return result;
+  },
 };
 
 module.exports = nextConfig;
