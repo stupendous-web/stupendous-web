5fe5bf2dcb2de7a2fa92fbc8f6f61698b8d985df
stupendous-web <topher@stupendousweb.com>
2022-07-09 11:23:47 -0700

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,19 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import { init } from "@socialgouv/matomo-next";
 
 import Pride from "../components/Pride";
 import Footer from "../components/Footer";
 import Menu from "../components/Menu";
 import Loading from "../components/Loading";
 import Cursor from "../components/Cursor";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init({ url: "https://analytics.stupendousweb.com/", siteId: 1 });
+  }, [0]);
   return (
     <Provider store={store}>
       <Component {...pageProps} />
