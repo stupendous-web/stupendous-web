e71541d06f3a474ff28e225189535e9f7c60ef4c
stupendous-web <topher@stupendousweb.com>
2022-07-10 16:57:34 -0700

    Think twice, code once.

--- components/Analytics.js
+++ components/Analytics.js
@@ -5,17 +5,19 @@ import axios from "axios";
 export default function Analytics({ site }) {
   const analytics = DWAnalytics({});
   useEffect(() => {
-    analytics.page().then((data) => {
-      const properties = data.payload.properties;
-      axios.post("https://analytics.stupendousweb.com/api/capture", {
-        site: site,
-        anonymousId: data.payload.anonymousId,
-        path: properties.path,
-        referrer: properties.referrer,
-        height: properties.height,
-        width: properties.width,
-      });
-    });
+    analytics
+      .page((data) => {
+        const properties = data.payload.properties;
+        axios.post("https://analytics.stupendousweb.com/api/capture", {
+          site: site,
+          anonymousId: data.payload.anonymousId,
+          path: properties.path,
+          referrer: properties.referrer,
+          height: properties.height,
+          width: properties.width,
+        });
+      })
+      .then();
   }, []);
   return;
 }
