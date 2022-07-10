22216e1b8d4e976fc8ccf01c58a7b2be1b3c90dc
stupendous-web <topher@stupendousweb.com>
2022-07-10 13:28:49 -0700

    Think twice, code once.

--- analytics.js
+++ analytics.js
@@ -2,19 +2,19 @@ import { Analytics } from "analytics";
 import axios from "axios";
 
 const analytics = Analytics({});
-analytics
-  .page((data) => {
-    const properties = data.payload.properties;
-    axios
-      .post("https://analytics-lac.vercel.app/api/capture", {
-        path: properties.path,
-        referrer: properties.referrer,
-        search: properties.search,
-        height: properties.height,
-        width: properties.width,
-      })
-      .then((response) => {
-        console.log(response.data);
-      });
-  })
-  .then();
+analytics.page().then((data) => {
+  const properties = data.payload.properties;
+  axios
+    .post("https://analytics-lac.vercel.app/api/capture", {
+      path: properties.path,
+      referrer: properties.referrer,
+      height: properties.height,
+      width: properties.width,
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import uikit from "uikit";
 import "../styles/uikit/uikit.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -6,7 +5,6 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import { init } from "@socialgouv/matomo-next";
 import "../analytics";
 
 import Pride from "../components/Pride";
@@ -16,9 +14,6 @@ import Loading from "../components/Loading";
 import Cursor from "../components/Cursor";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    init({ url: "https://analytics.stupendousweb.com/", siteId: 1 });
-  }, []);
   return (
     <Provider store={store}>
       <Component {...pageProps} />
