8848c613920f20b5408511fff77a4c8ea05d3264
stupendous-web <topher@stupendousweb.com>
2022-07-24 10:58:44 -0700

    Think twice, code once.

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,25 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Analytics as DWAnalytics } from "analytics";
 import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function Analytics({ site }) {
   const router = useRouter();
+  const [id, setId] = useState();
+
+  const sendEndTimestamp = () => {
+    axios.post("https://analytics-api.stupendousweb.com/pagaveiws", {
+      id: id,
+      _method: "patch",
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("unload", sendEndTimestamp);
+    return window.removeEventListener("unload", sendEndTimestamp);
+  }, []);
+
   useEffect(() => {
     if (!router.isReady) return;
     const { analytics } = router.query;
     if (process.env.NODE_ENV !== "development" && analytics !== "off") {
       DWAnalytics({}).page((data) => {
         const properties = data.payload.properties;
-        axios.post("https://analytics-api.stupendousweb.com/pageviews", {
-          site: site,
-          anonymous_id: data.payload.anonymousId,
-          path: properties.path,
-          referrer: properties.referrer || "Direct",
-          search: properties.search,
-          height: properties.height,
-          width: properties.width,
-        });
+        axios
+          .post("https://analytics-api.stupendousweb.com/pageviews", {
+            site: site,
+            anonymous_id: data.payload.anonymousId,
+            path: properties.path,
+            referrer: properties.referrer || "Direct",
+            search: properties.search,
+            height: properties.height,
+            width: properties.width,
+          })
+          .then((response) => {
+            setId(response.data);
+          });
       });
     }
   }, [router.isReady]);
