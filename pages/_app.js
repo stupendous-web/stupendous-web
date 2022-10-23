547f2824e4955969330490fb7ec1d2bcfced38cf
stupendous-web <topher@stupendousweb.com>
2022-10-22 18:00:30 -0700

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -16,11 +16,12 @@ import StupendousAnalytics from "stupendous-analytics";
 
 function MyApp({ Component, pageProps }) {
   console.log(
-    "  ___ _                          _            __      __   _    \n" +
+    "%c  ___ _                          _            __      __   _    \n" +
       " / __| |_ _  _ _ __  ___ _ _  __| |___ _  _ __\\ \\    / /__| |__ \n" +
       " \\__ \\  _| || | '_ \\/ -_) ' \\/ _` / _ \\ || (_-<\\ \\/\\/ / -_) '_ \\\n" +
       " |___/\\__|\\_,_| .__/\\___|_||_\\__,_\\___/\\_,_/__/ \\_/\\_/\\___|_.__/.COM\n" +
-      "              |_| "
+      "              |_| ",
+    "color: #ec008c"
   );
   return (
     <>
