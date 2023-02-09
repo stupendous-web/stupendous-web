0318a0e1195684e7e648aa7c4038ea857799918b
stupendousweb <topher@stupendousweb.com>
2023-02-09 14:18:16 -0800

    Think twice, code once.

---
+++ next-sitemap.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const axios = require("axios");
+
+module.exports = {
+  siteUrl: "https://stupendousweb.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  additionalPaths: async () => {
+    const result = [];
+    await axios
+      .get("https://public-api.wordpress.com/rest/v1.1/sites/67222684/posts")
+      .then((response) => {
+        response.data?.posts?.map((post) => {
+          result.push({
+            loc: "/articles/" + post.slug,
+            changefreq: "daily",
+            priority: 0.7,
+            lastmod: new Date(post.date).toISOString(),
+          });
+        });
+      });
+    return result;
+  },
+};
--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const axios = require("axios");
-
 const nextConfig = {
   reactStrictMode: true,
-  siteUrl: "https://stupendousweb.com",
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-  additionalPaths: async () => {
-    const result = [];
-    await axios
-      .get("https://public-api.wordpress.com/rest/v1.1/sites/67222684/posts")
-      .then((response) => {
-        response.data?.posts?.map((post) => {
-          result.push({
-            loc: "/articles/" + post.slug,
-            changefreq: "daily",
-            priority: 0.7,
-            lastmod: new Date(post.date).toISOString(),
-          });
-        });
-      });
-    return result;
-  },
   images: {
     domains: ["kinwebdesign.files.wordpress.com"],
   },
