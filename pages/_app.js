2681a167bac6cf41f3a87cdbeae54222850dd1e8
stupendousweb <topher@stupendousweb.com>
2023-03-18 12:44:05 -0700

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+import { useRouter } from "next/router";
 import uikit from "uikit";
 import "../styles/uikit/uikit.css";
 import "remixicon/fonts/remixicon.css";
 import { Provider } from "../lib/context";
+import * as gtag from "../lib/gtag";
 
 import Navigation from "../components/Navigation";
 import Contact from "../components/Contact";
@@ -11,9 +16,21 @@ import Menu from "../components/Menu";
 import Logo from "../components/Logo";
 import Loading from "../components/Loading";
 import Cursor from "../components/Cursor";
-import StupendousAnalytics from "stupendous-analytics-next";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   console.log(
     "%c ███  █████ █   █ ████  ████ █   █ ███   ███  █   █  ███  █     █ ████ ████\n" +
       "█       █   █   █ █   █ █    ██  █ █  █ █   █ █   █ █     █     █ █    █   █\n" +
@@ -25,7 +42,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <StupendousAnalytics site={"topher@stupendousweb.com"} />
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Provider>
         <Component {...pageProps} />
         <Footer />
