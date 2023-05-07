f0d35f48a79ef6efbc11fd3511ca3de1c99ac0e8
stupendousweb <topher@stupendousweb.com>
2023-05-07 08:47:17 -0700

    Think twice, code once.

--- pages/2022.js
+++ pages/2022.js
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import Head from "next/head";
+import NextLink from "next/link";
 import { useGlobal } from "../lib/context";
 
 import CTA from "../components/CTA";
 
 import isometric from "../images/isometrics/isometric-2-1.png";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  List,
+  ListItem,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import { Parallax } from "react-scroll-parallax";
 
 export default function Review2022() {
   const { setIsLoading } = useGlobal();
@@ -16,100 +28,95 @@ export default function Review2022() {
           2022 Review | Software Development Services | Stupendous Web
         </title>
       </Head>
-      <div className={"uk-section uk-section-xlarge uk-padding-remove-bottom"}>
-        <div className={"uk-container uk-container-small"}>
-          <div className={"uk-flex-middle"} data-uk-grid={""}>
-            <div className={"uk-width-1-3@s uk-visible@s"}>
-              <Image
-                src={isometric}
-                alt={"Software Development Services"}
-                priority
-                onLoadingComplete={() => setIsLoading(false)}
-              />
-            </div>
-            <div className={"uk-width-2-3@s"}>
-              <p className={"uk-text-justify uk-dropcap monospace"}>
-                At the end of every year I try my best with various tools and
-                methods to assess who I am and my values to better serve myself,
-                my communities, and my clients. Here are some conclusions I drew
-                from 2022.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={"uk-section"}>
-        <div className={"uk-container"}>
-          <h1
-            className={"uk-heading-2xlarge uk-text-right"}
-            data-uk-parallax={"x: 0, -800"}
-          >
+      <Container maxW={"container.xl"} pt={[16, 32]} pb={8}>
+        <Flex direction={["column", "row"]} align={"center"}>
+          <Box w={["100%", "33.33%"]} pr={4} mb={8}>
+            <Image
+              src={isometric}
+              alt={"Software Development Services"}
+              priority
+              onLoadingComplete={() => setIsLoading(false)}
+            />
+          </Box>
+          <Box w={["100%", "66.66%"]}>
+            <Text as={"code"}>
+              At the end of every year I try my best with various tools and
+              methods to assess who I am and my values to better serve myself,
+              my communities, and my clients. Here are some conclusions I drew
+              from 2022.
+            </Text>
+          </Box>
+        </Flex>
+      </Container>
+      <Parallax translateY={[0, 0]} translateX={[250, -100]}>
+        <Container maxW={"container.xl"} py={8}>
+          <Heading as={"h1"} size={"4xl"}>
             2022 Review
-          </h1>
-        </div>
-      </div>
-      <div className={"uk-section"}>
-        <div className={"uk-container uk-container-small"}>
-          <div data-uk-grid={""}>
-            <div className={"uk-width-1-3@s"} />
-            <div className={"uk-width-2-3@s"}>
-              <ul className={"uk-list"}>
-                <li>🤝 6 amazing new clients</li>
-                <li>☕ ~1,095 cups of coffee</li>
-              </ul>
-              <p>
-                I ask all my clients what their values are because I think it
-                comes up in what all of us do. According to{" "}
-                <a
-                  href={"https://personalvalu.es"}
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  this test
-                </a>{" "}
-                at the end of 2022 I valued{" "}
-                <i>
-                  meaningful work, tolerance, equality, creativity, and
-                  authenticity
-                </i>
-                . Building software makes me happy but when I build something
-                meaningful to someone it gives me true happiness. Building
-                meaningful web and phone apps helps my clients create a more
-                diverse and tolerant world through inventiveness.
-              </p>
-              <p>
-                I&apos;m also a mediator, INFP, or at least according to{" "}
-                <a
-                  href={"https://www.16personalities.com/"}
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  Meyer Briggs
-                </a>
-                . I identify as someone who tends to be open-minded and creative
-                while also taking care in what I do.
-              </p>
-              <p>
-                I love working with and connecting with people who want to see
-                their values shine through their projects. My software
-                development services are centered heavily around this. I&apos;d
-                love to hear about what&apos;s driving you in this new year. We
-                can also connect over music; follow me on{" "}
-                <a
-                  href={
-                    "https://open.spotify.com/user/128910259?si=49165d893098424f"
-                  }
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  Spotify
-                </a>{" "}
-                to see what I&apos;ve been listening to. Happy new year!
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+          </Heading>
+        </Container>
+      </Parallax>
+      <Container maxW={"container.xl"} pt={8} pb={[16, 32]}>
+        <Flex justify={"flex-end"}>
+          <Box w={["100%", "66.66%"]}>
+            <List mb={4} __css={{ columns: 1 }}>
+              <ListItem py={2}>🤝 6 amazing new clients</ListItem>
+              <ListItem py={2}>☕ ~1,095 cups of coffee</ListItem>
+            </List>
+            <Text mb={4}>
+              I ask all my clients what their values are because I think it
+              comes up in what all of us do. According to{" "}
+              <Link
+                as={NextLink}
+                href={"https://personalvalu.es"}
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                this test
+              </Link>{" "}
+              at the end of 2022 I valued{" "}
+              <i>
+                meaningful work, tolerance, equality, creativity, and
+                authenticity
+              </i>
+              . Building software makes me happy but when I build something
+              meaningful to someone it gives me true happiness. Building
+              meaningful web and phone apps helps my clients create a more
+              diverse and tolerant world through inventiveness.
+            </Text>
+            <Text mb={4}>
+              I&apos;m also a mediator, INFP, or at least according to{" "}
+              <Link
+                as={NextLink}
+                href={"https://www.16personalities.com/"}
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                Meyer Briggs
+              </Link>
+              . I identify as someone who tends to be open-minded and creative
+              while also taking care in what I do.
+            </Text>
+            <Text mb={4}>
+              I love working with and connecting with people who want to see
+              their values shine through their projects. My software development
+              services are centered heavily around this. I&apos;d love to hear
+              about what&apos;s driving you in this new year. We can also
+              connect over music; follow me on{" "}
+              <Link
+                as={NextLink}
+                href={
+                  "https://open.spotify.com/user/128910259?si=49165d893098424f"
+                }
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                Spotify
+              </Link>{" "}
+              to see what I&apos;ve been listening to. Happy new year!
+            </Text>
+          </Box>
+        </Flex>
+      </Container>
       <CTA />
     </>
   );
