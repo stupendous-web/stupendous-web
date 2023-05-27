c8be0eed6db9eb60d9c469f8cefd2ba3a9bc4315
stupendous-web <topher@stupendousweb.com>
2023-05-27 08:25:41 -0700

    Think twice, code once.

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,14 @@ export default function Document() {
   return (
     <Html lang={"en"}>
       <Head>
+        {/* https://nextjs.org/docs/messages/next-script-for-ga */}
         {process.env.NODE_ENV !== "development" && (
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy={"afterInteractive"}
             />
-            <Script id={"google-analytics"}>
+            <Script id={"google-analytics"} strategy={"afterInteractive"}>
               {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
