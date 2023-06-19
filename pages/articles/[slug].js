728fe93530f3b54afa4a16a6a768b5244f6ed7f8
stupendous-web <topher@stupendousweb.com>
2023-06-18 17:39:43 -0700

    Think twice, code once.

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -14,14 +14,18 @@ import {
   Heading,
   Text,
   Link,
+  GridItem,
+  Card,
+  SimpleGrid,
+  useColorMode,
 } from "@chakra-ui/react";
-
 import CTA from "../../components/CTA";
-
 dayjs.extend(relativeTime);
 
 export default function Article({ article }) {
   const { setIsLoading } = useGlobal();
+  const { colorMode } = useColorMode();
+
   const tag = Object.keys(article.tags)[0]?.toLowerCase();
 
   return (
@@ -61,71 +65,88 @@ export default function Article({ article }) {
         publisherLogo={"https://stupendousweb.com/images/logo.png"}
         isAccessibleForFree={true}
       />
-      <Container maxW={"container.lg"} pt={32} pb={8}>
-        {article?.featured_image && (
-          <AspectRatio maxW={"100%"} ratio={16 / 9} mb={8}>
-            <Image
-              src={article?.featured_image}
-              alt={article?.title}
-              fill
-              style={{ objectFit: "cover" }}
-              priority
-              onLoadingComplete={() => setIsLoading(false)}
-            />
-          </AspectRatio>
-        )}
-        <Flex align={"center"} mb={4}>
-          <Image
-            src={article?.author?.avatar_URL}
-            alt={article?.author?.name}
-            height={64}
-            width={64}
-            style={{ borderRadius: 32, marginRight: "1rem" }}
-          />
-          <Box>
-            <Link as={NextLink} href={article?.author?.URL}>
-              {article?.author?.name}
-            </Link>
-            <Text fontSize={"sm"} color={"gray.300"} mb={0}>
-              published{" "}
-              <Text
-                as={"time"}
-                dateTime={dayjs(article?.date).format("YYYY-MM-DD")}
-              >
-                {dayjs(article?.date).from(dayjs())}
-              </Text>{" "}
-              {tag && (
-                <span>
-                  &middot;{" "}
-                  <Link
-                    as={NextLink}
-                    href={`/articles/tags/${tag}`}
-                    color={"gray.300"}
-                  >
-                    {tag}
-                  </Link>{" "}
-                </span>
-              )}
-              &middot; {Math.round(article?.content.split(" ").length / 200)}{" "}
-              min read
-            </Text>
-          </Box>
-        </Flex>
-        <Heading as={"h1"}>{article?.title}</Heading>
-        <Box
-          dangerouslySetInnerHTML={{ __html: article?.content }}
-          sx={{
-            "& p": {
-              marginBottom: "1rem",
-            },
-            "& h2": {
-              color: "white",
-              fontSize: "2.25rem",
-              fontWeight: "bold",
-              marginBottom: "2rem",
-            },
-          }}
-        />
+      <Container maxW={"container.xl"} pt={[4, 16]}>
+        <SimpleGrid columns={8} spacing={2}>
+          <GridItem colSpan={[8, 3]}>
+            <Card>
+              <Heading as={"h1"} fontSize={["2rem", "4rem"]} mb={4}>
+                {article?.title}
+              </Heading>
+              <Flex align={"center"} mb={4}>
+                <Image
+                  src={article?.author?.avatar_URL}
+                  alt={article?.author?.name}
+                  height={64}
+                  width={64}
+                  style={{ borderRadius: 32, marginRight: "1rem" }}
+                />
+                <Box>
+                  <Link as={NextLink} href={article?.author?.URL}>
+                    {article?.author?.name}
+                  </Link>
+                  <Text fontSize={"sm"} color={"gray.300"} mb={0}>
+                    published{" "}
+                    <Text
+                      as={"time"}
+                      dateTime={dayjs(article?.date).format("YYYY-MM-DD")}
+                    >
+                      {dayjs(article?.date).from(dayjs())}
+                    </Text>{" "}
+                    {tag && (
+                      <span>
+                        &middot;{" "}
+                        <Link
+                          as={NextLink}
+                          href={`/articles/tags/${tag}`}
+                          color={"gray.300"}
+                        >
+                          {tag}
+                        </Link>{" "}
+                      </span>
+                    )}
+                    &middot;{" "}
+                    {Math.round(article?.content.split(" ").length / 200)} min
+                    read
+                  </Text>
+                </Box>
+              </Flex>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={3}>
+            {article?.featured_image && (
+              <AspectRatio ratio={1}>
+                <Image
+                  src={article?.featured_image}
+                  alt={article?.title}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  priority
+                  onLoadingComplete={() => setIsLoading(false)}
+                />
+              </AspectRatio>
+            )}
+          </GridItem>
+          <GridItem colSpan={2} />
+          <GridItem colSpan={2} />
+          <GridItem colSpan={[8, 6]}>
+            <Card variant={"transparent"}>
+              <Box
+                dangerouslySetInnerHTML={{ __html: article?.content }}
+                sx={{
+                  "& p": {
+                    mb: 4,
+                  },
+                  "& h2": {
+                    color: colorMode === "dark" ? "white" : "gray.900",
+                    fontSize: "2rem",
+                    fontWeight: "bold",
+                    mb: 8,
+                  },
+                }}
+              />
+            </Card>
+          </GridItem>
+        </SimpleGrid>
       </Container>
       <CTA />
     </>
