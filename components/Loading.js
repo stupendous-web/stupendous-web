28316b5562e93c91a1294066b0ac9bc7002a92b0
stupendous-web <topher@stupendousweb.com>
2023-06-29 15:59:33 -0700

    Think twice, code once.

--- components/Loading.js
+++ components/Loading.js
@@ -1,40 +1,25 @@
 import { useGlobal } from "../lib/context";
-import { Cursor, Typewriter } from "react-simple-typewriter";
-import { Box, Text, Flex, Container } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Text, Flex, Spinner, Center } from "@chakra-ui/react";
 
 export default function Loading() {
-  const [isTyping, setIsTyping] = useState(true);
   const { isLoading } = useGlobal();
 
   return (
-    <Box
+    <Center
+      bg={"primary.500"}
+      h={"100vh"}
       w={"100%"}
       position={"fixed"}
       top={0}
       left={0}
-      className={isLoading || isTyping ? undefined : "slide-out-left"}
-      style={{
-        animationDelay: ".5s",
-      }}
+      className={isLoading ? undefined : "fade-out"}
     >
-      <Flex align={"center"} bg={"primary.500"} h={"100vh"}>
-        <Container maxW={"container.sm"}>
-          <Text as={"code"} color={"white"}>
-            [topher@stupendous ~]$&nbsp;
-            <Typewriter
-              words={[
-                "If you want to build community, build stupendous software.",
-              ]}
-              typeSpeed={25}
-              onLoopDone={() => {
-                setTimeout(() => setIsTyping(false), 2500);
-              }}
-            />
-            <Cursor cursorStyle={"█"} />
-          </Text>
-        </Container>
-      </Flex>
-    </Box>
+      <Box>
+        <Flex justify={"center"}>
+          <Spinner color={"white"} size={"xl"} mb={8} />
+        </Flex>
+        <Text color={"white"}>Hang in there.</Text>
+      </Box>
+    </Center>
   );
 }
