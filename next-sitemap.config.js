f1c5e62f018db8e22734f08161a905d26defe9e0
stupendous-web <topher@stupendousweb.com>
2023-08-07 15:30:38 -0700

    Think twice, code once.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,7 +8,7 @@ module.exports = {
   generateIndexSitemap: false,
   autoLastmod: false,
   priority: false,
-  exclude: ["/articles", "/services/expo"],
+  exclude: ["/articles"],
   changefreq: "monthly",
   additionalPaths: async () => {
     const result = [];
